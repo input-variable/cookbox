2304a7c2785405a4607688fc71b75892b954bfd4
nakotami <personal@doublefold.co.uk>
2021-03-06 18:21:54 +0000

    Create bloat.js

---
+++ bloat.js
@@ -0,0 +1,25 @@
+function loadlights(){
+  var x = document.cookie;
+
+  if (x == "lights=off"){
+    document.documentElement.style.setProperty('--text', '#FFFFFF');
+    document.documentElement.style.setProperty('--nottext', '#000000');
+
+  } else {
+    document.documentElement.style.setProperty('--text', '#000000');
+    document.documentElement.style.setProperty('--nottext', '#FFFFFF');
+  }
+}
+
+function togglelights(){
+  var x = document.cookie;
+  
+  if (x == "lights=off"){
+  	document.cookie = "lights=on";
+  } else {
+  	document.cookie = "lights=off";
+  }
+  loadlights();
+}
+
+loadlights();
